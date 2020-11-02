6e2a3e431fffc89d9f64dfad5894c2fa2488cbae
Benjamin Sommervoll <puixdk@gmail.com>
2020-11-02 08:02:49 +0100

    Arbejde på pizza class

--- IntelliJ-Project/src/main/java/Main.java
+++ IntelliJ-Project/src/main/java/Main.java
@@ -4,7 +4,14 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
 	public static void main(String[] args) {
-		launch(args);
+
+	}
+
+	private static void initiatePizza() {
+		Pizza pizza1 = new Pizza("Vesuvio", "tomatsauce, ost, skinke og oregano", 57);
+		Pizza pizza2 = new Pizza("Amerikaner", "tomatsauce, ost, oksefars og oregano", 53, true);
+		Pizza pizza3 = new Pizza("Cacciatore", "tomatsauce, ost, pepperoni og oregano", 57);
+		Pizza pizza4 = new Pizza("Carbona", "tomatsauce, ost, kødsauce, spaghetti, cocktailpølser og oregano", 63, true);
 	}
 
 	@Override
--- IntelliJ-Project/src/main/java/Pizza.java
+++ IntelliJ-Project/src/main/java/Pizza.java
@@ -1,2 +1,26 @@
 public class Pizza {
+	private String name, components;
+	private int price;
+	private boolean news;
+	private static int pizzaCounter = 0;
+
+	public Pizza(String name, String components, int price, boolean news) {
+		this.name = name;
+		this.components = components;
+		this.price = price;
+		this.news = news;
+		pizzaCounter++;
+	}
+
+	public Pizza(String name, String components, int price) {
+		this.name = name;
+		this.components = components;
+		this.price = price;
+		this.news = false;
+		pizzaCounter++;
+	}
+
+	public static int getPizzaCounter() {
+		return pizzaCounter;
+	}
 }
